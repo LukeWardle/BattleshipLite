cb0240331274e802cdef4ea1464f26cb178e7318
Luke Wardle <l.wardle@live.co.uk>
2022-05-22 03:27:32 +0100

    Add project files.

---
+++ BattleshipLogic/GameLogic.cs
@@ -0,0 +1,56 @@
+﻿using BattleshipLibrary.Models;
+using BattleshipLogic.Models;
+using System;
+using System.Collections.Generic;
+
+namespace BattleshipLogic
+{
+    public static class GameLogic
+    {
+        public static void InitializeGrid(PlayerInfoModel model)
+        {
+            List<string> letters = new List<string>
+            {
+                "A",
+                "B",
+                "C",
+                "D",
+                "E"
+            };
+
+            List<int> numbers = new List<int>
+            {
+                1,
+                2,
+                3,
+                4,
+                5
+            };
+
+            foreach (string letter in letters)
+            {
+                foreach (int number in numbers)
+                {
+                    AddGridSpot(model, letter, number);
+                }
+            }
+        }
+
+        private static void AddGridSpot(PlayerInfoModel model, string letter, int number)
+        {
+            GridSpotModel spot = new GridSpotModel
+            {
+                SpotLetter = letter,
+                SpotNumber = number,
+                Status = GridSpotStatus.Empty
+            };
+
+            model.ShotGrid.Add(spot);
+        }
+
+        public static bool PlaceShip(PlayerInfoModel model, string location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,76 @@
+﻿using BattleshipLibrary.Models;
+using BattleshipLogic;
+using System;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeMessage();
+
+            PlayerInfoModel player1 = CreatePlayer("Player 1");
+            PlayerInfoModel player2 = CreatePlayer("Player 2");
+
+
+            Console.ReadLine();
+        }
+
+        private static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Battleship Lite");
+            Console.WriteLine("Created by Luke Wardle");
+            Console.WriteLine("Part of the Tim Corey C# Masterclass");
+            Console.WriteLine();
+
+        }
+
+        private static PlayerInfoModel CreatePlayer(string playerTitle)
+        {
+            PlayerInfoModel output = new PlayerInfoModel();
+
+            Console.WriteLine($"Player information for {playerTitle}");
+
+            // Ask the user for their name.
+            output.UsersName = AskForUsersName();
+
+            // Load up the shot grid.
+            GameLogic.InitializeGrid(output);
+
+            // Ask the user for their ship placement.
+            PlaceShips(output);
+
+            // Clear.
+            Console.Clear();
+
+            return output;
+        }
+
+        private static string AskForUsersName()
+        {
+            Console.Write("What is your name: ");
+            string output = Console.ReadLine();
+
+            return output;
+        }
+
+        private static void PlaceShips(PlayerInfoModel model)
+        {
+            // Hardcoded.
+            do
+            {
+                Console.Write($"Where do you want to place ship number {model.ShipLocations.Count + 1}: ");
+                string location = Console.ReadLine();
+
+                bool isValidLocation = GameLogic.PlaceShip(model, location);
+
+                if (isValidLocation == false)
+                {
+                    Console.WriteLine("That was not a valid location. Please try again.");
+                }
+
+            } while (model.ShipLocations.Count < 5);
+        }
+    }
+}
